9c1c1ebcd630050f83b9f464a3d3e85bbec0d611
Denis <thesocot@mail.ru>
2022-11-28 20:43:10 +0300

    Fixed bugs

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,9 @@
 "use strict";
 import load from './modules/load';
 window.addEventListener('DOMContentLoaded',()=>{
-    load('#input','.grid',"#search",'8d-xBcwWl0Pkze8PAKvsEkk1Qg_iQcFsZOPCllxkS9Q');
+    load({
+        inputData:'#input',
+        gridData:'.grid', 
+        searchData:"#search",
+        accesKey:'8d-xBcwWl0Pkze8PAKvsEkk1Qg_iQcFsZOPCllxkS9Q'});
 });
